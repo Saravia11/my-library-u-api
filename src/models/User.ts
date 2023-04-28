37cbc137c81cc0bce744fb5648c8c83d4661d23a
Saravia11 <diego.saravia@focusservices.com>
2023-04-27 23:08:43 -0600

    feat: User model with mongoose

---
+++ src/models/User.ts
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+
+export default model(
+  "User",
+  new Schema({
+    name: {
+      required: true,
+      type: String,
+    },
+    last_name: {
+      required: true,
+      type: String,
+    },
+    carnet: {
+      required: true,
+      type: String,
+    },
+    hash: {
+      required: true,
+      type: String,
+    },
+    role: {
+      required: true,
+      type: String,
+      enum: ["admin", "student"],
+    },
+  })
+);
