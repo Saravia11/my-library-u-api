dd2575d713554365d49242e14b8377ed7fb75272
Saravia11 <diego.saravia@focusservices.com>
2023-05-01 05:12:21 -0600

    chore: removing useless try...catch

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -10,23 +10,19 @@ export default class UserController {
    * POST: /
    */
   static post = asyncHandler(async (req: Request<any, any, PostInput>, res) => {
-    try {
-      const { password, ...restUser } = req.body;
-      const hash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+    const { password, ...restUser } = req.body;
+    const hash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
-      const user = new User({
-        ...restUser,
-        hash,
-      });
-      const data = await user.save();
+    const user = new User({
+      ...restUser,
+      hash,
+    });
+    const data = await user.save();
 
-      res.status(201).json({
-        message: "User created successfully",
-        data,
-      });
-    } catch (error) {
-      res.status(500).json({ message: error });
-    }
+    res.status(201).json({
+      message: "User created successfully",
+      data,
+    });
   });
 
   /**
@@ -45,28 +41,22 @@ export default class UserController {
    * GET: /:id
    */
   static getId = asyncHandler(async (req, res) => {
-    try {
-      const id = req.params.id;
+    const id = req.params.id;
 
-      const user = await User.findById(id).catch((err) => {
-        err.status;
+    const user = await User.findById(id);
+
+    if (!user) {
+      res.status(404).json({
+        message: "User not found",
       });
-
-      if (!user) {
-        res.status(404).json({
-          message: "User not found",
-        });
-        return;
-      }
-
-      const { hash, ..._user } = user.toJSON();
-      res.status(200).json({
-        message: "User found",
-        data: _user,
-      });
-    } catch (error) {
-      res.status(500).json({ message: error });
+      return;
     }
+
+    const { hash, ..._user } = user.toJSON();
+    res.status(200).json({
+      message: "User found",
+      data: _user,
+    });
   });
 
   /**
